ad4c2817dcb0539e2c6a462aff3953ef4f83b5bd
Fabricio Porras Morera <faporras05@hotmail.com>
2024-04-03 16:22:01 -0700

    Add more graphs option

--- front-end/src/UI/sections/Stats.tsx
+++ front-end/src/UI/sections/Stats.tsx
@@ -1,6 +1,7 @@
 import "./Stats.css";
 import { useEffect, useState } from "react";
 import { BarChart } from "@mui/x-charts/BarChart";
+import { PieChart } from '@mui/x-charts/PieChart';
 
 interface StatsProps {
   chat: File[];
@@ -9,17 +10,19 @@ interface StatsProps {
 }
 
 export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
-  const [selectedOption, setSelectedOption] =
+  const [selectedAnalysisOption, setSelectedAnalysisOption] =
     useState<string>("Conversaciones");
-    const [textResult, setTextResult] = useState<{
+  const [selectedGraphOption, setSelectedGraphOption] =
+    useState<string>("Barras");
+  const [textResult, setTextResult] = useState<{
     categories: string[];
     confidence: number[];
   }>({ categories: [], confidence: [] });
-    const [audioResult, setAudioResult] = useState<{
+  const [audioResult, setAudioResult] = useState<{
     categories: string[];
     confidence: number[];
   }>({ categories: [], confidence: [] });
-    const [imgsResult, setImgsResult] = useState<{
+  const [imgsResult, setImgsResult] = useState<{
     categories: string[];
     confidence: number[];
   }>({ categories: [], confidence: [] });
@@ -31,16 +34,20 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
   //192.168.20.88 GANTE
   //192.168.1.188 CASA
   useEffect(() => {
-      textAnalysis();
-      imagsAnalysis();
-      audioAnalysis();
+    textAnalysis();
+    imagsAnalysis();
+    audioAnalysis();
   }, []);
 
   useEffect(() => {
-    if(selectedOption ==="Conversaciones" && textResult.categories.length>0){
-      handleNavOption("Conversaciones")
+    if (
+      selectedAnalysisOption === "Conversaciones" &&
+      textResult.categories.length > 0
+    ) {
+      handleNavAnalysisOption("Conversaciones");
     }
-}, ),textResult; 
+  }),
+    textResult;
 
   const processData = (data: any, analysis: string) => {
     const categories = Object.keys(data).map(function (clave) {
@@ -49,18 +56,18 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
     const confidence = Object.keys(data).map(function (clave) {
       return data[clave];
     });
-    switch (analysis){
+    switch (analysis) {
       case "text":
-        console.log("texto")
-        setTextResult({ categories, confidence })
+        console.log("texto");
+        setTextResult({ categories, confidence });
         break;
       case "audio":
-        console.log("audio")
-        setAudioResult({ categories, confidence })
+        console.log("audio");
+        setAudioResult({ categories, confidence });
         break;
       case "img":
-        console.log("imgs")
-        setImgsResult({ categories, confidence })
+        console.log("imgs");
+        setImgsResult({ categories, confidence });
         break;
     }
   };
@@ -113,11 +120,10 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
         },
       });
 
-
       if (response.ok) {
         const result = await response.json();
-        console.log(result)
-        return result
+        console.log(result);
+        return result;
       } else {
         console.error("Request error:", response.status);
       }
@@ -140,8 +146,8 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
         if (response.ok) {
           const data = await response.json();
           const result = await genericTextAnalysis(data.transcript);
-          console.log(result)
-          processData(result,"audio");
+          console.log(result);
+          processData(result, "audio");
         } else {
           console.error("Request error:", response.status);
         }
@@ -168,8 +174,8 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
         if (response.ok) {
           const data = await response.json();
           const result = await genericTextAnalysis(data.description);
-          console.log(result)
-          processData(result,"img");
+          console.log(result);
+          processData(result, "img");
         } else {
           console.error("Request error:", response.status, response.body);
         }
@@ -179,26 +185,29 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
     }
   }
 
-  const handleNavOption = async (identifier: string): Promise<void> => {
-    setSelectedOption(identifier);
+  const handleNavAnalysisOption = async (identifier: string): Promise<void> => {
+    setSelectedAnalysisOption(identifier);
     switch (identifier) {
       case "Conversaciones":
-        setChartResult(textResult)
+        setChartResult(textResult);
         break;
       case "Audios":
-        setChartResult(audioResult)
+        setChartResult(audioResult);
         break;
       case "Imagenes":
-        setChartResult(imgsResult)
+        setChartResult(imgsResult);
         break;
       case "Todos":
-        setChartResult({categories: [],confidence: []})
+        setChartResult({ categories: [], confidence: [] });
         break;
       default:
         break;
     }
   };
-  
+
+  const handleNavGraphOption = async (identifier: string): Promise<void> => {
+    setSelectedGraphOption(identifier);
+  };
 
   return (
     <section className="stats-section">
@@ -206,32 +215,36 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
         <ul className="lateral-navigation">
           <li>
             <a
-              className={selectedOption === "Conversaciones" ? "selected" : ""}
-              onClick={() => handleNavOption("Conversaciones")}
+              className={
+                selectedAnalysisOption === "Conversaciones" ? "selected" : ""
+              }
+              onClick={() => handleNavAnalysisOption("Conversaciones")}
             >
               Conversaciones
             </a>
           </li>
           <li>
             <a
-              className={selectedOption === "Audios" ? "selected" : ""}
-              onClick={() => handleNavOption("Audios")}
+              className={selectedAnalysisOption === "Audios" ? "selected" : ""}
+              onClick={() => handleNavAnalysisOption("Audios")}
             >
               Audios
             </a>
           </li>
           <li>
             <a
-              className={selectedOption === "Imagenes" ? "selected" : ""}
-              onClick={() => handleNavOption("Imagenes")}
+              className={
+                selectedAnalysisOption === "Imagenes" ? "selected" : ""
+              }
+              onClick={() => handleNavAnalysisOption("Imagenes")}
             >
               Imagenes
             </a>
           </li>
           <li>
             <a
-              className={selectedOption === "Todos" ? "selected" : ""}
-              onClick={() => handleNavOption("Todos")}
+              className={selectedAnalysisOption === "Todos" ? "selected" : ""}
+              onClick={() => handleNavAnalysisOption("Todos")}
             >
               Todos
             </a>
@@ -240,26 +253,67 @@ export const Stats: React.FC<StatsProps> = ({ chat, imgs, audios }) => {
         <div className="stats-container">
           {chartResult.categories.length > 0 &&
           chartResult.confidence.length > 0 ? (
-            <BarChart
-              xAxis={[
-                {
-                  id: "barCategories",
-                  data: chartResult.categories,
-                  scaleType: "band",
-                },
-              ]}
-              series={[
-                {
-                  data: chartResult.confidence,
-                },
-              ]}
-              width={600}
-              height={500}
-            />
+            selectedGraphOption === "Barras" ? (
+              <BarChart
+                xAxis={[
+                  {
+                    id: "barCategories",
+                    data: chartResult.categories,
+                    scaleType: "band",
+                  },
+                ]}
+                series={[
+                  {
+                    data: chartResult.confidence,
+                  },
+                ]}
+                width={600}
+                height={500}
+              />
+            ) : (
+              <PieChart
+                series={[
+                  {
+                    data: chartResult.confidence.map((value, index) => ({
+                      id: index,
+                      value: value,
+                      label: chartResult.categories[index], 
+                    })),
+                  },
+                ]}
+                width={450}
+                height={300}
+              />
+            )
           ) : (
-            <p className="stats-paragraph">No hay datos disponibles para mostrar para {selectedOption}.</p>
+            <p className="stats-paragraph">
+              No hay datos disponibles para mostrar para{" "}
+              {selectedAnalysisOption}.
+            </p>
           )}
         </div>
+        <ul className="lateral-right-navigation">
+          <li>
+            <a
+              className={
+                selectedGraphOption === "Barras" ? "selectedGraph" : ""
+              }
+              onClick={() => handleNavGraphOption("Barras")}
+            >
+              Barras
+            </a>
+          </li>
+          <li>
+            <a
+              className={
+                selectedGraphOption === "Pastel" ? "selectedGraph" : ""
+              }
+              onClick={() => handleNavGraphOption("Pastel")}
+            >
+              Pastel
+            </a>
+          </li>
+        </ul>
       </div>
     </section>
   );
